f415a2531ba9a101aeb3e50763531150d8050c0d
einfalles <dn5jg@virginia.edu>
2019-02-10 19:30:19 +0100

    webhook setup

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        return '', 200
+    else:
+        abort(400)
+
 if __name__ == '__main__':
     app.run()
